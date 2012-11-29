e89ed8b1e1a4058c15d189335a39cb4384314ba9
Dary, Dominik <ddary@ebay.com>
2012-11-29 14:55:41 +0100

    test improvement

--- irb_android.sh
+++ irb_android.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 TEST_SERVER_PORT=34777 
-MOBILE_APP_PACKAGE='sh.calaba.demoproject.test'
-adb shell am instrument -e class sh.calaba.instrumentationbackend.InstrumentationBackend -w $MOBILE_APP_PACKAGE/sh.calaba.instrumentationbackend.CalabashInstrumentationTestRunner &
-sleep 7
-IRBRC=.irbrc TEST_SERVER_PORT=$TEST_SERVER_PORT irb
+PACKAGE_NAME='sh.calabash.demoproject'
+MAIN_ACTIVITY='sh.calabash.demoproject.HelloAndroidActivity'
+
+IRBRC=.irbrc TEST_SERVER_PORT=$TEST_SERVER_PORT PACKAGE_NAME=$PACKAGE_NAME MAIN_ACTIVITY=$MAIN_ACTIVITY irb
